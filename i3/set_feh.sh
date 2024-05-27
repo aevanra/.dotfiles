d50738304c27ac14fccad0e50a0da50451c9f5e1
aevanra <aevan.armus@gmail.com>
2024-05-26 23:36:53 -0400

    i3 and picom updates -- removing neofetch :(

--- i3/set_feh.sh
+++ i3/set_feh.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+landscape1=$(ls /home/aevan/Pictures/wallpapers/landscape/ | echo /home/aevan/Pictures/wallpapers/landscape/$(shuf -n 1))
+landscape2=$(ls /home/aevan/Pictures/wallpapers/landscape/ | echo /home/aevan/Pictures/wallpapers/landscape/$(shuf -n 1))
+
+while [ landscape1 == landscape2 ]; do
+    landscape2 = $(ls /home/aevan/Pictures/wallpapers/landscape/ | echo /home/aevan/Pictures/wallpapers/landscape/$(shuf -n 1))
+done
+
 feh --bg-fill $(ls /home/aevan/Pictures/wallpapers/portrait/ | echo /home/aevan/Pictures/wallpapers/portrait/$(shuf -n 1)) \
-    --bg-fill $(ls /home/aevan/Pictures/wallpapers/landscape/ | echo /home/aevan/Pictures/wallpapers/landscape/$(shuf -n 1)) \
-    --bg-fill $(ls /home/aevan/Pictures/wallpapers/landscape/ | echo /home/aevan/Pictures/wallpapers/landscape/$(shuf -n 1))
+    --bg-fill $landscape1 \
+    --bg-fill $landscape2
--- i3/setup_polybar.sh
+++ i3/setup_polybar.sh
@@ -1,4 +1,9 @@
-  for m in $(polybar --list-monitors | cut -d":" -f1); do
+if $(pgrep -x polybar > /dev/null); then
+  killall -q polybar
+  sleep 1
+fi
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload main &
     done
 else
