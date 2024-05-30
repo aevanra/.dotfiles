b4f0b4e9bc0c8baf5576cab69c9d3939b5ef2544
aevanra <aevan.armus@gmail.com>
2024-05-30 09:08:00 -0400

    feh comment fix

--- i3/set_feh.sh
+++ i3/set_feh.sh
@@ -1,4 +1,4 @@
-#! /bin/zsh
+#! /bin/zsh 
 
 landscape1=$(ls /home/aevan/Pictures/wallpapers/landscape/ | echo /home/aevan/Pictures/wallpapers/landscape/$(shuf -n 1))
 landscape2=$(ls /home/aevan/Pictures/wallpapers/landscape/ | echo /home/aevan/Pictures/wallpapers/landscape/$(shuf -n 1))
