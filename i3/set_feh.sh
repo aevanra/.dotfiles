1aaf39d49e1ff30f2790529e3eee7615b66b57e5
aevanra <aevan.armus@gmail.com>
2024-06-05 22:53:30 -0400

    syntax error correction

--- i3/set_feh.sh
+++ i3/set_feh.sh
@@ -1,7 +1,7 @@
 landscape1=$(ls /home/aevan/Pictures/wallpapers/landscape/ | echo /home/aevan/Pictures/wallpapers/landscape/$(shuf -n 1))
 landscape2=$(ls /home/aevan/Pictures/wallpapers/landscape/ | echo /home/aevan/Pictures/wallpapers/landscape/$(shuf -n 1))
 
-while [ landscape1 == landscape2 ]; do
+while [ "$landscape1" == "$landscape2" ]; do
     landscape2 = $(ls /home/aevan/Pictures/wallpapers/landscape/ | echo /home/aevan/Pictures/wallpapers/landscape/$(shuf -n 1))
 done
 
