8343f222008a0651af560d6d4e5aead6ef2c8843
aevanra <aevan.armus@gmail.com>
2024-12-03 10:27:53 -0500

    monitor update

--- i3/set_i3_monitors.sh
+++ i3/set_i3_monitors.sh
@@ -1,8 +1,8 @@
 
 # Monitors left to right -- DP-0, HDMI-1, DP-2
 xrandr --output DP-0 --auto --pos 0x0 --rotate left --rate 75
-xrandr --output DP-2 --auto --pos 3000x120 --rotate normal --rate 144
-xrandr --output HDMI-1 --auto --pos 1080x480 --rotate normal --rate 144
+xrandr --output DP-2 --auto --pos 3640x120 --rotate normal --rate 240
+xrandr --output HDMI-1 --auto --pos 1080x120 --rotate normal --rate 144
 
 xrandr --output HDMI-0 --off
 
