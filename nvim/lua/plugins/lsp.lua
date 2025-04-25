5e5de42aa3b92947ff7224e275520df0872912e2
aevanra <aevan.armus@gmail.com>
2025-04-25 09:57:37 -0400

    removed lsp-zero

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,47 +1,59 @@
 return {
     {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v3.x",
+        "neovim/nvim-lspconfig",
         dependencies = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" },
-            { "williamboman/mason.nvim" },
-            { "williamboman/mason-lspconfig.nvim" },
-
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-buffer" },
-            { "hrsh7th/cmp-path" },
-            { "L3MON4D3/LuaSnip" },
-            { "saadparwaiz1/cmp_luasnip" },
-            { "rafamadriz/friendly-snippets" },
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp", -- For LSP capabilities
         },
         config = function()
-            local lsp_zero = require("lsp-zero")
-            local cmp = require("cmp")
-            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            local lspconfig = require("lspconfig")
+            local mason = require("mason")
+            local mason_lspconfig = require("mason-lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            -- Load friendly snippets
-            require("luasnip.loaders.from_vscode").lazy_load()
+            mason.setup()
 
-            -- LSP Attach logic
-            lsp_zero.on_attach(function(client, bufnr)
-                local opts = { buffer = bufnr, remap = false }
-            end)
-
-            -- Setup Mason
-            require("mason").setup({})
-            require("mason-lspconfig").setup({
+            mason_lspconfig.setup({
+                ensure_installed = { "lua_ls", "ruff" },
                 handlers = {
-                    lsp_zero.default_setup,
-                    lua_ls = function()
-                        local lua_opts = lsp_zero.nvim_lua_ls()
-                        require("lspconfig").lua_ls.setup(lua_opts)
+                    function(server_name)
+                        lspconfig[server_name].setup({
+                            capabilities = capabilities,
+                            on_attach = function(client, bufnr)
+                                local opts = { buffer = bufnr, silent = true, noremap = true }
+
+                                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+                                -- Format on Save
+                                if client.server_capabilities.documentFormattingProvider then
+                                    vim.api.nvim_create_autocmd("BufWritePre", {
+                                        buffer = bufnr,
+                                        callback = function()
+                                            vim.lsp.buf.format({ async = false })
+                                        end,
+                                    })
+                                end
+                            end
+                        })
                     end,
-                    ruff = function()
-                        require("lspconfig").ruff.setup({
+
+                    ["lua_ls"] = function()
+                        lspconfig.lua_ls.setup({
+                            capabilities = capabilities,
+                            settings = {
+                                Lua = {
+                                    diagnostics = { globals = { "vim" } },
+                                    workspace = { checkThirdParty = false },
+                                },
+                            },
+                        })
+                    end,
+
+                    ["ruff"] = function()
+                        lspconfig.ruff.setup({
                             capabilities = capabilities,
                             settings = {
                                 enable = true,
@@ -56,50 +68,6 @@ return {
                     end,
                 },
             })
-
-            -- Setup CMP
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                }, {
-                    { name = "buffer" },
-                }),
-                capabilities = capabilities,
-                formatting = lsp_zero.cmp_format(),
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-e>"] = cmp.mapping.close(),
-                    ["<C-o>"] = cmp.mapping.complete(),
-                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-                    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-                    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                }),
-            })
         end,
     },
-
-    -- LSP Formatting
-    { "onsails/lspkind.nvim" },
-
-    -- Command Completion
-    {
-        "smolck/command-completion.nvim",
-        opts = {}
-    },
-
-    -- Snippets
-    { "L3MON4D3/LuaSnip" },
-
-    -- Shellcheck
-    { "itspriddle/vim-shellcheck" },
 }
-
-
