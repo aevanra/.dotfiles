f0b1c83b7c530738d50c342149b03a946b7f28a3
aevanra <aevan.armus@gmail.com>
2025-04-30 16:04:11 -0400

    whichkey and ts fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,4 +3,5 @@ require("config/remap")
 require("config/set")
 require("lazy").setup("plugins")
 vim.cmd(":Copilot disable")
+vim.cmd(":TSEnable highlight")
 vim.cmd.colorscheme "catppuccin-macchiato"
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,8 @@ return {
     {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
+        lazy = false,
+        priority = 1000,
         opts = {
             -- A list of parser names, or "all" (the five listed parsers should always be installed)
             ensure_installed = { "vimdoc", "python", "c", "lua", "vim", "vimdoc", "query" },
@@ -18,11 +20,6 @@ return {
 
             highlight = {
                 enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
                 additional_vim_regex_highlighting = false,
             },
         }
